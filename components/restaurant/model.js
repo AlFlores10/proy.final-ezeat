3b8b920a1004cfb53f9495e5af745863cafafb8f
AlFlores10 <alberto.flores.1990@gmail.com>
2021-01-24 17:54:55 +0100

    Finish Restaurant-Model

---
+++ components/restaurant/model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const RestaurantSchema = new mongoose.Schema({
+
+    name: { 
+        type: String,
+        required: true,
+        maxlength: 30
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        maxlength: 50
+    },
+    password: { 
+        type: String, 
+        required: true,
+        minlength: 7,
+        maxlength: 100
+    },
+    adress: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    delivery: {
+        type: Boolean,
+        required: true
+    }
+});
+
+
+module.exports = mongoose.model('Restaurant', CustomerSchema);
