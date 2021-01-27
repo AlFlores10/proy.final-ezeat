3631adca9b5a652ee9220e5f8f84e32650bfad12
AlFlores10 <alberto.flores.1990@gmail.com>
2021-01-27 04:54:14 +0100

    Feature Finish Login & Token

---
+++ components/config/config.js
@@ -0,0 +1,45 @@
+const Customer = require('../customer/model.js');
+const Restaurant = require('../restaurant/model.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jwt-simple');
+const secret = 'elsecretodemigato';
+const moment = require('moment');
+
+
+module.exports.loginUser = async (req, res, next) => {
+    try {
+        let user = '';
+        if (req.body.role = 'customer') {
+            user = await Customer.findOne({ email: req.body.email });
+        };
+        if (req.body.role = 'restaurant') {
+            user = await Restaurant.findOne({ email: req.body.email });
+        };
+        if (user) {
+            const iguales = bcrypt.compareSync(req.body.password, user.password);
+            if (iguales) {
+                res.json({ sucess: createToken(user) });
+
+            } else {
+                res.json({ error: 'Error en usuario y/o contraseña' });
+            }
+
+        } else {
+            res.json({ error: 'Error en usuario y/o contraseña' });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    next();
+};
+
+
+const createToken = (user) => {
+    const payload = {
+        usuarioId: user.id,
+        createdAt: moment().unix(),
+        expiredAt: moment().add(59, 'minutes').unix()
+    }
+
+    return jwt.encode(payload, secret);
+}
